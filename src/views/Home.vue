6854ed3ef8d705e1fea71ada71127efd68aca9e3
Stanislau Balan <stanislau.balan@fabros-team.com>
2021-11-24 17:48:56 +0300

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="home">
+    asdf
+    <div>
+      {{ $store.state }}
+    </div>
+    <h1>HOME</h1>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
---
+++ vue.config.js
@@ -0,0 +1,27 @@
+const ModuleFederationPlugin = require("webpack").container.ModuleFederationPlugin;
+// const deps = require('./package.json').dependencies;
+
+module.exports = {
+    devServer: {
+        port: 8085,
+    },
+    publicPath: process.env.NODE_ENV === 'development' ? "/" : "https://profile-wmf.vercel.app/",
+    configureWebpack: {
+        plugins: [
+            new ModuleFederationPlugin({
+                name: "home",
+                filename: "remoteHomeEntry.js",
+                exposes: {
+                    "./Home": "./src/views/Home.vue",
+                },
+                // shared: {
+                //     vuex: {
+                //         eager: true,
+                //         singleton: true,
+                //         requiredVersion: deps.vuex
+                //     }
+                // }
+            }),
+        ],
+    },
+};
